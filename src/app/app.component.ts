6a57a2f138625314c7dea86fee3874efac1e4a61
Rashmi Ulhas Dusane <rashmi.dusane@hof-university.de>
2021-09-11 16:20:40 +0200

    Initialized angular and bootstrap and created first basic structure for UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import data from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Assignment';
+  totalFollowers = "23004";
+
+public data:any = data;
+
+ngOnInit(){
+
+
+}
 }
