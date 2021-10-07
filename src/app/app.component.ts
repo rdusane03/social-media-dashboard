2d895facf1943ab061b806188d7a8e89cef83e4c
Rashmi Ulhas Dusane <rashmi.dusane@hof-university.de>
2021-10-07 08:18:56 +0200

    Created Overview cards for Light Theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { Data } from '@angular/router';
 import data from '../assets/data.json';
 
 @Component({
@@ -7,13 +9,38 @@ import data from '../assets/data.json';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  public data:any =data;
   title = 'Assignment';
   totalFollowers = "23004";
+  
 
-public data:any = data;
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2) { }
 
-ngOnInit(){
+  ngOnInit() {
+    
+  }
+   //const toggleSwitch = this.document.querySelector('#toggle-button');
 
+  switchTheme(e:any) {
+    console.log(e.classList)
+    //console.log(e.classList)
+    
+
+    if(e.classList.contains('active'))
+    {
+     
+     
+      this.document.documentElement.setAttribute('data-theme', 'dark');
+      console.log(this.document.documentElement.getAttribute('data-theme'))
+    }
+      else {
+        
+        document.documentElement.setAttribute('data-theme', 'light');
+      }    
+  }
+  
+ // toggleSwitch.addEventListener('change', switchTheme, false);
 
 }
-}
+
+export type Mode = 'light-mode' | 'dark-mode';
